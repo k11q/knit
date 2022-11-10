938759ebaa4c4b8f30f1abd00c7f90ba371d07e4
Khairul Haaziq <rhycoz@yahoo.com>
2022-11-10 19:10:36 +0800

    add left side panel component

---
+++ components/LeftSidePanel/LeftSidePanel.vue
@@ -0,0 +1,67 @@
+<template>
+  <div
+    class="border-r w-60 flex flex-col flex-none h-full overflow-x-hidden overflow-y-scroll z-10 bg-white"
+  >
+    <div
+      class="h-10 border-b flex-none flex flex-row items-center px-2 sticky top-0 bg-white"
+    >
+      <div
+        class="px-2 h-full items-center flex flex-row cursor-pointer"
+        :class="{
+          'font-medium text-black': leftPanelStore.activeTab === 'layers',
+          'text-neutral-400': leftPanelStore.activeTab !== 'layers',
+        }"
+        @click="leftPanelStore.activeTab = 'layers'"
+      >
+        <span>Layers</span>
+      </div>
+      <div
+        class="px-2 h-full items-center flex flex-row cursor-pointer"
+        :class="{
+          'font-medium text-black': leftPanelStore.activeTab === 'data',
+          'text-neutral-400': leftPanelStore.activeTab !== 'data',
+        }"
+        @click="leftPanelStore.activeTab = 'data'"
+      >
+        <span>Data</span>
+      </div>
+      <div
+        class="px-2 h-full items-center flex flex-row cursor-pointer"
+        :class="{
+          'font-medium text-black': leftPanelStore.activeTab === 'assets',
+          'text-neutral-400': leftPanelStore.activeTab !== 'assets',
+        }"
+        @click="leftPanelStore.activeTab = 'assets'"
+      >
+        <span>Assets</span>
+      </div>
+    </div>
+    <!--layers tab content -->
+    <div
+      class="flex flex-col w-full h-fit"
+      v-show="leftPanelStore.activeTab === 'layers'"
+    >
+      <TreeBrowser :nodes="selectToi.data" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useCounterStore } from "../stores/counter";
+import { useLeftPanelStore } from "../stores/leftPanelStore";
+
+const selectToi = useCounterStore();
+const leftPanelStore = useLeftPanelStore();
+</script>
+
+<script>
+export default {
+  name: "LeftSidePanel",
+  props: {},
+  data() {
+    return {};
+  },
+  emits: "",
+  methods: {},
+};
+</script>
--- pages/about.vue
+++ pages/about.vue
@@ -8,51 +8,7 @@
     <!--Main section-->
     <div class="flex flex-row justify-between">
       <!--Left panel section-->
-      <div
-        class="border-r w-60 flex flex-col flex-none h-full overflow-x-hidden overflow-y-scroll z-10 bg-white"
-      >
-        <div
-          class="h-10 border-b flex-none flex flex-row items-center px-2 sticky top-0 bg-white"
-        >
-          <div
-            class="px-2 h-full items-center flex flex-row cursor-pointer"
-            :class="{
-              'font-medium text-black': leftPanelStore.activeTab === 'layers',
-              'text-neutral-400': leftPanelStore.activeTab !== 'layers',
-            }"
-            @click="leftPanelStore.activeTab = 'layers'"
-          >
-            <span>Layers</span>
-          </div>
-          <div
-            class="px-2 h-full items-center flex flex-row cursor-pointer"
-            :class="{
-              'font-medium text-black': leftPanelStore.activeTab === 'data',
-              'text-neutral-400': leftPanelStore.activeTab !== 'data',
-            }"
-            @click="leftPanelStore.activeTab = 'data'"
-          >
-            <span>Data</span>
-          </div>
-          <div
-            class="px-2 h-full items-center flex flex-row cursor-pointer"
-            :class="{
-              'font-medium text-black': leftPanelStore.activeTab === 'assets',
-              'text-neutral-400': leftPanelStore.activeTab !== 'assets',
-            }"
-            @click="leftPanelStore.activeTab = 'assets'"
-          >
-            <span>Assets</span>
-          </div>
-        </div>
-        <!--layers tab content -->
-        <div
-          class="flex flex-col w-full h-fit"
-          v-show="leftPanelStore.activeTab === 'layers'"
-        >
-          <TreeBrowser :nodes="selectToi.data" />
-        </div>
-      </div>
+      <LeftSidePanel />
       <!--Canvas section-->
       <div
         id="container"
@@ -2736,8 +2692,11 @@ function wheel() {
   event.preventDefault();
   console.log("e.deltaY = " + event.deltaY);
   console.log("e.deltaX = " + event.deltaX);
+  console.log("e.touches = " + event.touches);
 
-  addaSquare.scale += event.deltaY * -0.01;
+  if (event.deltaX === 0) {
+    addaSquare.scale += event.deltaY * -0.01;
+  }
 
   // Restrict scale
   addaSquare.scale = Math.min(Math.max(0.125, scale), 4);
