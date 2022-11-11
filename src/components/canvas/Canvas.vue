606e16c7f2db7d4e7105e18039c6a91adc64fb0a
Khairul Haaziq <rhycoz@yahoo.com>
2022-11-11 16:52:49 +0800

    resizer anti scale

--- src/components/canvas/Canvas.vue
+++ src/components/canvas/Canvas.vue
@@ -22,74 +22,89 @@
       <UIBrowser :nodes="selectToi.data" />
     </div>
     <div
-      v-if="selectToi.selectedBox && !canvasFF.isDragging"
-      class="absolute pointer-events-none"
+      class="fixed top-0 left-0 w-0 h-0 overflow-visible"
       :style="{
-        left: selectToi.selectedBoxHTMLX + addaSquare.offsetLeft + 'px',
-        top: selectToi.selectedBoxHTMLY + addaSquare.offsetTop + 'px',
-        height: selectToi.selectedBoxData.height
-          ? selectToi.selectedBoxData.height * addaSquare.scale + 'px'
-          : document.querySelector(
-              `[data-id=${selectToi.selectedBoxData.parent}]`
-            ).getBoundingClientRect.height,
-        width: selectToi.selectedBoxData.width
-          ? selectToi.selectedBoxData.width * addaSquare.scale + 'px'
-          : 'auto',
+        transform: `translate3D(${addaSquare.offsetLeft}px, ${addaSquare.offsetTop}px, 0px) rotate(${addaSquare.rotation}deg) scale(${addaSquare.scale})`,
       }"
     >
-      <div class="pointer-events-auto">
-        <p
-          class="absolute left-0 right-0 top-full mt-2 flex flex-row justify-center flex-nowrap"
-        >
-          <span
-            class="bg-blue-600 text-white cursor-default px-1 py-0.5 rounded text-[11px] flex-nowrap flex"
+      <div
+        v-if="selectToi.selectedBox && !canvasFF.isDragging"
+        class="absolute pointer-events-none"
+        :style="{
+          left: selectToi.selectedBoxHTMLX + 'px',
+          top: selectToi.selectedBoxHTMLY + 'px',
+          height: selectToi.selectedBoxData.height
+            ? selectToi.selectedBoxData.height + 'px'
+            : document.querySelector(
+                `[data-id=${selectToi.selectedBoxData.parent}]`
+              ).getBoundingClientRect.height,
+          width: selectToi.selectedBoxData.width
+            ? selectToi.selectedBoxData.width + 'px'
+            : 'auto',
+        }"
+      >
+        <div class="pointer-events-auto">
+          <p
+            class="absolute left-0 right-0 top-full mt-2 flex flex-row justify-center flex-nowrap"
           >
-            {{
-              selectToi.selectedBoxData.width
-                ? selectToi.selectedBoxData.width
-                : "Fill"
-            }}
-            x
-            {{
-              selectToi.selectedBoxData.height
-                ? selectToi.selectedBoxData.height
-                : "Fill"
-            }}
-          </span>
-        </p>
-        <div
-          @mousedown.stop.prevent="resizeStore.resizeTop"
-          class="absolute bottom-full h-0.5 bg-blue-600 w-full hover:cursor-row-resize"
-        />
-        <div
-          @mousedown.stop.prevent="resizeStore.resizeBottom"
-          class="absolute top-full h-0.5 bg-blue-600 w-full hover:cursor-row-resize"
-        />
-        <div
-          @mousedown.stop.prevent="resizeStore.resizeLeft"
-          class="absolute right-full w-0.5 bg-blue-600 h-full hover:cursor-col-resize"
-        />
-        <div
-          @mousedown.stop.prevent="resizeStore.resizeRight"
-          class="absolute left-full w-0.5 bg-blue-600 h-full hover:cursor-col-resize"
-        />
+            <span
+              class="bg-blue-600 text-white cursor-default px-1 py-0.5 rounded text-[11px] flex-nowrap flex"
+            >
+              {{
+                selectToi.selectedBoxData.width
+                  ? selectToi.selectedBoxData.width
+                  : "Fill"
+              }}
+              x
+              {{
+                selectToi.selectedBoxData.height
+                  ? selectToi.selectedBoxData.height
+                  : "Fill"
+              }}
+            </span>
+          </p>
+          <div
+            @mousedown.stop.prevent="resizeStore.resizeTop"
+            class="absolute bottom-full bg-blue-600 w-full hover:cursor-row-resize"
+            :style="{ height: `${(2 * 1) / addaSquare.scale}px` }"
+          />
+          <div
+            @mousedown.stop.prevent="resizeStore.resizeBottom"
+            class="absolute top-full bg-blue-600 w-full hover:cursor-row-resize"
+            :style="{ height: `${(2 * 1) / addaSquare.scale}px` }"
+          />
+          <div
+            @mousedown.stop.prevent="resizeStore.resizeLeft"
+            class="absolute right-full bg-blue-600 h-full hover:cursor-col-resize"
+            :style="{ width: `${(2 * 1) / addaSquare.scale}px` }"
+          />
+          <div
+            @mousedown.stop.prevent="resizeStore.resizeRight"
+            class="absolute left-full bg-blue-600 h-full hover:cursor-col-resize"
+            :style="{ width: `${(2 * 1) / addaSquare.scale}px` }"
+          />
 
-        <div
-          @mousedown.stop.prevent="resizeStore.resizeTopLeft"
-          class="absolute -top-1 -left-1 h-2 w-2 bg-white border-blue-600 border hover:cursor-nwse-resize"
-        />
-        <div
-          @mousedown.stop.prevent="resizeStore.resizeTopRight"
-          class="absolute -top-1 -right-1 h-2 w-2 bg-white border-blue-600 border hover:cursor-nesw-resize"
-        />
-        <div
-          @mousedown.stop.prevent="resizeStore.resizeBottomRight"
-          class="absolute -bottom-1 -right-1 h-2 w-2 bg-white border-blue-600 border hover:cursor-nwse-resize"
-        />
-        <div
-          @mousedown.stop.prevent="resizeStore.resizeBottomLeft"
-          class="absolute -bottom-1 -left-1 h-2 w-2 bg-white border-blue-600 border hover:cursor-nesw-resize"
-        />
+          <div
+            @mousedown.stop.prevent="resizeStore.resizeTopLeft"
+            class="absolute -top-1 -left-1 h-2 w-2 bg-white border-blue-600 border hover:cursor-nwse-resize"
+            :style="{ transform: `scale( ${1 / addaSquare.scale})` }"
+          />
+          <div
+            @mousedown.stop.prevent="resizeStore.resizeTopRight"
+            class="absolute -top-1 -right-1 h-2 w-2 bg-white border-blue-600 border hover:cursor-nesw-resize"
+            :style="{ transform: `scale( ${1 / addaSquare.scale})` }"
+          />
+          <div
+            @mousedown.stop.prevent="resizeStore.resizeBottomRight"
+            class="absolute -bottom-1 -right-1 h-2 w-2 bg-white border-blue-600 border hover:cursor-nwse-resize"
+            :style="{ transform: `scale( ${1 / addaSquare.scale})` }"
+          />
+          <div
+            @mousedown.stop.prevent="resizeStore.resizeBottomLeft"
+            class="absolute -bottom-1 -left-1 h-2 w-2 bg-white border-blue-600 border hover:cursor-nesw-resize"
+            :style="{ transform: `scale( ${1 / addaSquare.scale})` }"
+          />
+        </div>
       </div>
     </div>
     <div class="absolute inset-0 overflow-visible pointer-events-none">
