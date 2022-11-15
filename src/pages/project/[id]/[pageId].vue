6af5ea712f0c62089f62a7e392006e8ac80487ef
Khairul Haaziq <rhycoz@yahoo.com>
2022-11-15 16:11:29 +0800

    delete layout

--- src/pages/project/[id]/[pageId].vue
+++ src/pages/project/[id]/[pageId].vue
@@ -2,7 +2,7 @@
 
 <script setup>
 const route = useRoute();
-const paramsId = route.params.Id;
+const paramsId = route.params.id;
 const paramsPageId = route.params.pageId;
 // When accessing /posts/1, route.params.id will be 1
 </script>
