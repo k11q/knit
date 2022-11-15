d91d9d99a1c2363a88d7a27af399b43a776eddda
Khairul Haaziq <rhycoz@yahoo.com>
2022-11-16 02:10:49 +0800

    add auth middleware

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "nuxt-headlessui"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@vueuse/nuxt",
+    "nuxt-headlessui",
+    "@nuxtjs/supabase",
+  ],
   buildModules: ["@pinia/nuxt"],
   headlessui: {
     prefix: "Headless",
--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -11,4 +11,12 @@
 import { useCounterStore } from "@/stores/counter";
 
 const selectToi = useCounterStore();
+
+const client = useSupabaseClient();
+const { data: project } = await useAsyncData("project", async () => {
+  const { data } = await client.from("project").select("*");
+  return data;
+});
+
+console.log("data =" + project.value);
 </script>
