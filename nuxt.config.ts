d82bfb370b6e66d550542b363d50426e1a276a22
Khairul Haaziq <rhycoz@yahoo.com>
2022-11-16 09:55:12 +0800

    remove supabase pkg2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: [
-    "@nuxtjs/tailwindcss",
-    "@vueuse/nuxt",
-    "nuxt-headlessui",
-    "@nuxtjs/supabase",
-  ],
-  buildModules: ["@pinia/nuxt", "@nuxtjs/supabase"],
+  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "nuxt-headlessui"],
+  buildModules: ["@pinia/nuxt"],
   headlessui: {
     prefix: "Headless",
   },
