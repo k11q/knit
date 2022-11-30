0389135bb564d2784e8aeda7eb6bf4e7f2b33043
Khairul Haaziq <rhycoz@yahoo.com>
2022-12-01 06:50:10 +0800

    add frame label

---
+++ src/components/designer/canvas/FrameLabel.vue
@@ -0,0 +1,45 @@
+<template>
+  <template v-for="frame in frames">
+    <div
+      :style="{
+        left: useGetElementRect(frame.id)?.x + 'px',
+        top: useGetElementRect(frame.id)?.y + 'px',
+      }"
+      class="h-1 w-1 fixed overflow-visible -mt-5 will-change-transform pointer-events-auto cursor-default hover:text-[#6EB0E0] hover:opacity-100"
+      :class="{
+        'text-[#6EB0E0] opacity-100': selectToi.selectedBox === frame.id,
+        'text-[#FAFAFA] opacity-40': selectToi.selectedBox !== frame.id,
+      }"
+      @mousedown="canvasStore.dndWithoutParent($event, frame.id, frame.type)"
+      @mouseover="
+        () => {
+          useSetOutlineHover(frame.id);
+          selectToi.treeHoverId = node.id;
+        }
+      "
+      @mouseout="
+        () => {
+          selectToi.treeHover = false;
+          selectToi.treeHoverId = '';
+        }
+      "
+      @click="selectToi.changeSelected($event, frame.id, frame.type)"
+    >
+      {{ frame.name }}
+    </div>
+  </template>
+</template>
+
+<script setup>
+import { useSquareStore } from "@/stores/dataSquare";
+import { useCounterStore } from "@/stores/counter";
+import { storeCanvas } from "@/stores/storeCanvas";
+
+const squareStore = useSquareStore();
+const selectToi = useCounterStore();
+const canvasStore = storeCanvas();
+
+const props = defineProps({
+  frames: Array,
+});
+</script>
