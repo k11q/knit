a06fb8c3a89d16f4c2a0f44c3f673c02db863f71
Khairul Haaziq <rhycoz@yahoo.com>
2022-12-17 23:57:58 +0800

    fix: change max pinchzoom value

--- src/composables/pinch-zoom.ts
+++ src/composables/pinch-zoom.ts
@@ -13,13 +13,13 @@ export function usePinchZoom(event: WheelEvent) {
       event.deltaX === 0 &&
       event.ctrlKey &&
       addaSquare.scale >= 0.02 &&
-      addaSquare.scale <= 25.6
+      addaSquare.scale <= 256
     ) {
       clearTimeout(endPinchZoom);
       let xs = (event.clientX - addaSquare.offsetLeft) / addaSquare.scale;
       let ys = (event.clientY - addaSquare.offsetTop) / addaSquare.scale;
       addaSquare.scale += event.deltaY * -0.008 * addaSquare.scale;
-      addaSquare.scale = Math.max(0.02, Math.min(25.6, addaSquare.scale));
+      addaSquare.scale = Math.max(0.02, Math.min(256, addaSquare.scale));
       addaSquare.offsetLeft = event.clientX - xs * addaSquare.scale;
       addaSquare.offsetTop = event.clientY - ys * addaSquare.scale;
     } else {
