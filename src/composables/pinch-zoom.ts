c043e6ac8ed44fc82d7c3fe58f03c1cbadcd055e
Khairul Haaziq <rhycoz@yahoo.com>
2022-12-18 16:06:35 +0800

    fix: remove requestanimationframe for pinchzoom

--- src/composables/pinch-zoom.ts
+++ src/composables/pinch-zoom.ts
@@ -8,32 +8,29 @@ export function usePinchZoom(event: WheelEvent) {
   event.preventDefault();
   canvasStore.isPinchZoom = true;
 
-  function update() {
-    if (
-      event.deltaX === 0 &&
-      event.ctrlKey &&
-      addaSquare.scale >= 0.02 &&
-      addaSquare.scale <= 256
-    ) {
-      clearTimeout(endPinchZoom);
-      let xs = (event.clientX - addaSquare.offsetLeft) / addaSquare.scale;
-      let ys = (event.clientY - addaSquare.offsetTop) / addaSquare.scale;
-      addaSquare.scale += event.deltaY * -0.008 * addaSquare.scale;
-      addaSquare.scale = Math.max(0.02, Math.min(256, addaSquare.scale));
-      addaSquare.offsetLeft = event.clientX - xs * addaSquare.scale;
-      addaSquare.offsetTop = event.clientY - ys * addaSquare.scale;
-    } else {
-      clearTimeout(endPinchZoom);
-      addaSquare.offsetLeft += -event.deltaX * 0.7;
-      addaSquare.offsetTop += -event.deltaY * 0.7;
-    }
-    function endPinchZoom() {
-      setTimeout(() => {
-        canvasStore.isPinchZoom = false;
-      }, "400");
-    }
-    measuredLines().value = [];
-    endPinchZoom();
+  if (
+    event.deltaX === 0 &&
+    event.ctrlKey &&
+    addaSquare.scale >= 0.02 &&
+    addaSquare.scale <= 256
+  ) {
+    clearTimeout(endPinchZoom);
+    let xs = (event.clientX - addaSquare.offsetLeft) / addaSquare.scale;
+    let ys = (event.clientY - addaSquare.offsetTop) / addaSquare.scale;
+    addaSquare.scale += event.deltaY * -0.008 * addaSquare.scale;
+    addaSquare.scale = Math.max(0.02, Math.min(256, addaSquare.scale));
+    addaSquare.offsetLeft = event.clientX - xs * addaSquare.scale;
+    addaSquare.offsetTop = event.clientY - ys * addaSquare.scale;
+  } else {
+    clearTimeout(endPinchZoom);
+    addaSquare.offsetLeft += -event.deltaX * 0.7;
+    addaSquare.offsetTop += -event.deltaY * 0.7;
   }
-  requestAnimationFrame(update);
+  function endPinchZoom() {
+    setTimeout(() => {
+      canvasStore.isPinchZoom = false;
+    }, "400");
+  }
+  measuredLines().value = [];
+  endPinchZoom();
 }
