2b53a3df28ad6faa360785d0445bc81a0b97d2a3
Khairul Haaziq <rhycoz@yahoo.com>
2022-12-18 16:13:53 +0800

    fix: remove hoverdata and id when pinchzoom

--- src/composables/pinch-zoom.ts
+++ src/composables/pinch-zoom.ts
@@ -1,36 +1,44 @@
 import { useSquareStore } from "~~/src/stores/dataSquare";
 import { useCanvasStore } from "@/stores/canvas";
+import { useCounterStore, Node } from "../stores/counter";
 
 export function usePinchZoom(event: WheelEvent) {
   const addaSquare = useSquareStore();
   const canvasStore = useCanvasStore();
+  const selectToi = useCounterStore();
 
   event.preventDefault();
   canvasStore.isPinchZoom = true;
+  canvasStore.hoverData = {} as Node;
+  canvasStore.hoverId = "";
+  selectToi.treeHoverId = "";
 
-  if (
-    event.deltaX === 0 &&
-    event.ctrlKey &&
-    addaSquare.scale >= 0.02 &&
-    addaSquare.scale <= 256
-  ) {
-    clearTimeout(endPinchZoom);
-    let xs = (event.clientX - addaSquare.offsetLeft) / addaSquare.scale;
-    let ys = (event.clientY - addaSquare.offsetTop) / addaSquare.scale;
-    addaSquare.scale += event.deltaY * -0.008 * addaSquare.scale;
-    addaSquare.scale = Math.max(0.02, Math.min(256, addaSquare.scale));
-    addaSquare.offsetLeft = event.clientX - xs * addaSquare.scale;
-    addaSquare.offsetTop = event.clientY - ys * addaSquare.scale;
-  } else {
-    clearTimeout(endPinchZoom);
-    addaSquare.offsetLeft += -event.deltaX * 0.7;
-    addaSquare.offsetTop += -event.deltaY * 0.7;
+  function update() {
+    if (
+      event.deltaX === 0 &&
+      event.ctrlKey &&
+      addaSquare.scale >= 0.02 &&
+      addaSquare.scale <= 256
+    ) {
+      clearTimeout(endPinchZoom);
+      let xs = (event.clientX - addaSquare.offsetLeft) / addaSquare.scale;
+      let ys = (event.clientY - addaSquare.offsetTop) / addaSquare.scale;
+      addaSquare.scale += event.deltaY * -0.008 * addaSquare.scale;
+      addaSquare.scale = Math.max(0.02, Math.min(256, addaSquare.scale));
+      addaSquare.offsetLeft = event.clientX - xs * addaSquare.scale;
+      addaSquare.offsetTop = event.clientY - ys * addaSquare.scale;
+    } else {
+      clearTimeout(endPinchZoom);
+      addaSquare.offsetLeft += -event.deltaX * 0.7;
+      addaSquare.offsetTop += -event.deltaY * 0.7;
+    }
+    function endPinchZoom() {
+      setTimeout(() => {
+        canvasStore.isPinchZoom = false;
+      }, "400");
+    }
+    measuredLines().value = [];
+    endPinchZoom();
   }
-  function endPinchZoom() {
-    setTimeout(() => {
-      canvasStore.isPinchZoom = false;
-    }, "400");
-  }
-  measuredLines().value = [];
-  endPinchZoom();
+  requestAnimationFrame(update);
 }
